4e464a4a9b3ba490f8c8f03839432e3a1c5f3bbe
Justin Kenney <justinkenney13@gmail.com>
2018-11-16 20:52:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,15 +17,16 @@ class ApplicationController < Sinatra::Base
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+
+    artist_name = params[:artist_name]
     
     if Artist.find_by(name: artist_name)
       @artist = Artist.find_by(name: artist_name)
     else
       @artist = Artist.create(name: artist_name)
     end
-    
-    artist_name = params[:artist_name]
-    binding.pry
+
+    # binding.pry
     @song.update(name: params[:songs][:name], artist: @artist)
     @song.genres.clear
 
